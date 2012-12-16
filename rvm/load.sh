30488b84f88a5caccfab5009205de555aa0aa15d
Jordi Beltran <beltrachi@gmail.com>
2012-12-16 12:44:17 +0100

    Fix rvm load to not set and load ruby & gemset

--- rvm/load.sh
+++ rvm/load.sh
@@ -1,7 +1,4 @@
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
 
-rvm use ruby-1.8.7-p334@generic
-
-
 PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
 
