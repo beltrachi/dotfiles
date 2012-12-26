d3ef51b55df6dc65b99aba0ce6edb73f7832ef25
Jordi Beltran <beltrachi@gmail.com>
2012-12-26 22:31:25 +0100

    Make bash PS1 var conditional to rvm presence

--- bash/env.sh
+++ bash/env.sh
@@ -1,2 +1,7 @@
-export PS1='\[\033[01;32m\]\u@\h\[\033[01;34m\] \W \[\e[0;31m\][$(~/.rvm/bin/rvm-prompt i v p g system)]\[\e[0;32m\] $(__git_ps1 "(%s)")\[\e[0;37m\] \$\[\033[00m\] '
+if [ -x ~/.rvm/bin/rvm-prompt ]; then
+    RVM_PART='\[\e[0;31m\][$(~/.rvm/bin/rvm-prompt i v p g system)]'
+else
+    RVM_PART=''
+fi
+export PS1='\[\033[01;32m\]\u@\h\[\033[01;34m\] \W '$RVM_PART'\[\e[0;32m\] $(__git_ps1 "(%s)")\[\e[0;37m\] \$\[\033[00m\] '
 
