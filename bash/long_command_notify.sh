0705ab10daf5e42f06a6116f7ed2feb7035c26ba
Jordi Beltran <beltrachi@gmail.com>
2013-11-17 01:18:57 +0100

    Add long_command_notify.sh

---
+++ bash/long_command_notify.sh
@@ -0,0 +1,9 @@
+# Script to notify on slow commands
+beep_if_long_time_past() {
+    LAST_COMMAND_DURATION=$(($(date +%s) - ${LAST_COMMAND_TIME}))
+    [[ ${LAST_COMMAND_DURATION} -gt 10 ]] && { notify-send "Script finished in ${LAST_COMMAND_DURATION} seconds" "$previous_command"; }
+    export LAST_COMMAND_TIME=
+}
+
+export PROMPT_COMMAND=beep_if_long_time_past
+trap 'previous_command=$this_command; this_command=$BASH_COMMAND && [ -z ${LAST_COMMAND_TIME} ] && export LAST_COMMAND_TIME=$(date +%s) ' DEBUG
