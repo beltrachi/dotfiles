28439a62bac5446261473396341762db4498b06d
Jordi Beltran <beltrachi@gmail.com>
2013-12-11 15:56:25 +0100

    ubuntu 12.04 setup

---
+++ ubuntu/scripts/setup.d/basics.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+apt-get install -y git-core chromium-browser curl openssh-server pidgin build-essential indicator-multiload tree
+update-rc.d ssh defaults
---
+++ ubuntu/scripts/setup.d/devel.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+apt-get install -y eatmydata virtualbox zlib1g-dev mysql-server git libxslt-dev libxml2-dev qt-sdk libmysqlclient-dev libssl-dev libreadline6 libreadline6-dev redis-server imagemagick
