155d349279ad6713f72f7e79a006b3e4614d945e
Jordi Beltran <beltrachi@gmail.com>
2013-12-11 16:46:50 +0100

    ubuntu setup - add vagrant

--- ubuntu/scripts/setup.d/devel.sh
+++ ubuntu/scripts/setup.d/devel.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 apt-get install -y eatmydata virtualbox zlib1g-dev mysql-server git libxslt-dev libxml2-dev qt-sdk libmysqlclient-dev libssl-dev libreadline6 libreadline6-dev redis-server imagemagick
+
+if [[ ! $(command -v vagrant) ]]; then
+  wget -O /tmp/vagrant_1.4.0_x86_64.deb https://dl.bintray.com/mitchellh/vagrant/vagrant_1.4.0_x86_64.deb
+  dpkg -i /tmp/vagrant_1.4.0_x86_64.deb
+fi
+
