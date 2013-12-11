ba271102cefbc1cfa9b8efb0b92032246c8b3db9
Jordi Beltran <beltrachi@gmail.com>
2013-12-11 16:47:02 +0100

    ubuntu setup - Added vim

--- ubuntu/scripts/setup.d/basics.sh
+++ ubuntu/scripts/setup.d/basics.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-apt-get install -y git-core chromium-browser curl openssh-server pidgin build-essential indicator-multiload tree
+apt-get install -y git-core chromium-browser curl openssh-server pidgin build-essential indicator-multiload tree vim
 update-rc.d ssh defaults
