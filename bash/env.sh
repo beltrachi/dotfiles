2f7962ad890b491b7db1ca44fe8d42dc92ce662c
Jordi Beltran <beltrachi@gmail.com>
2013-12-17 15:16:03 +0100

    Add env name

--- bash/env.sh
+++ bash/env.sh
@@ -1,7 +1,7 @@
 if [ -x ~/.rvm/bin/rvm-prompt ]; then
     RVM_PART='\[\e[0;31m\][$(~/.rvm/bin/rvm-prompt i v p g system)]'
 else
-    RVM_PART=''
+    RVM_PART='\[\e[0;31m\][env=$RAILS_ENV]'
 fi
 export PS1='\[\033[01;32m\]\u@\h\[\033[01;34m\] \W '$RVM_PART'\[\e[0;32m\] $(__git_ps1 "(%s)")\[\e[0;37m\] \$\[\033[00m\] '
 
