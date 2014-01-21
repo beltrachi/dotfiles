8c0ebc6595968db4b45430e02f6d3396e18f479f
Jordi Beltran <beltrachi@gmail.com>
2014-01-21 10:07:10 +0100

    Ubuntu setup - add s3cmd

--- ubuntu/scripts/setup.d/devel.sh
+++ ubuntu/scripts/setup.d/devel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt-get install -y eatmydata virtualbox zlib1g-dev mysql-server git \
   libxslt-dev libxml2-dev qt-sdk libmysqlclient-dev libssl-dev libreadline6 \
-  libreadline6-dev redis-server imagemagick gitk nginx
+  libreadline6-dev redis-server imagemagick gitk nginx s3cmd
 
 if [[ ! $(command -v vagrant) ]]; then
   wget -O /tmp/vagrant_1.4.0_x86_64.deb https://dl.bintray.com/mitchellh/vagrant/vagrant_1.4.0_x86_64.deb
