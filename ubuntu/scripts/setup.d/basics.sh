ea5e16c128c7241921c547bbe1a8cbc491a286e5
Jordi Beltran <beltrachi@gmail.com>
2014-02-17 14:53:44 +0100

    Ubuntu setup - remove apport

--- ubuntu/scripts/setup.d/basics.sh
+++ ubuntu/scripts/setup.d/basics.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 apt-get install -y git-core chromium-browser curl openssh-server pidgin build-essential indicator-multiload tree vim
 update-rc.d ssh defaults
+
+# Uninstall Apport Error Reporting in Ubuntu
+apt-get purge apport
