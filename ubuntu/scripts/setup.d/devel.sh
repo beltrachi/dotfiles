c7fcc765e516ecbcc6bdc6610df3ec37aa03700d
Jordi Beltran <beltrachi@gmail.com>
2014-05-11 23:45:57 +0200

    ubuntu setup - add nodejs

--- ubuntu/scripts/setup.d/devel.sh
+++ ubuntu/scripts/setup.d/devel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt-get install -y eatmydata virtualbox zlib1g-dev mysql-server git \
   libxslt-dev libxml2-dev qt-sdk libmysqlclient-dev libssl-dev libreadline6 \
-  libreadline6-dev redis-server imagemagick gitk nginx s3cmd nodejs \
+  libreadline6-dev redis-server imagemagick gitk nginx s3cmd \
   libyaml-dev
 
 if [[ ! $(command -v vagrant) ]]; then
@@ -9,3 +9,6 @@ if [[ ! $(command -v vagrant) ]]; then
   dpkg -i /tmp/vagrant_1.4.0_x86_64.deb
 fi
 
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get install -y python-software-properties python g++ make nodejs
