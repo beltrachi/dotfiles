d58c843a8221a15d427a5a2495d1ca5cfb9e00cb
Jordi Beltran <beltrachi@gmail.com>
2014-06-17 09:25:22 +0200

    Add hour in bash ps1

--- bash/env.sh
+++ bash/env.sh
@@ -3,5 +3,5 @@ if [ -x ~/.rvm/bin/rvm-prompt ]; then
 else
     RVM_PART='\[\e[0;31m\][env=$RAILS_ENV]'
 fi
-export PS1='\[\033[01;32m\]\u@\h\[\033[01;34m\] \W '$RVM_PART'\[\e[0;32m\] $(__git_ps1 "(%s)")\[\e[0;37m\] \$\[\033[00m\] '
+export PS1='\[\033[1;30m\]\A \[\033[01;32m\]\u@\h\[\033[01;34m\] \W '$RVM_PART'\[\e[0;32m\] $(__git_ps1 "(%s)")\[\e[0;37m\] \$\[\033[00m\] '
 
