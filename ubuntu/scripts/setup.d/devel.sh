3e7a1e4e60fb5768e67ea2148061aba5d2d79128
Jordi Beltran <beltrachi@gmail.com>
2014-06-17 09:23:55 +0200

    Ubuntu - update vagrant to 1.6.2

--- ubuntu/scripts/setup.d/devel.sh
+++ ubuntu/scripts/setup.d/devel.sh
@@ -4,7 +4,7 @@ apt-get install -y eatmydata virtualbox zlib1g-dev mysql-server git \
   libreadline6-dev redis-server imagemagick gitk nginx s3cmd
 
 if [[ ! $(command -v vagrant) ]]; then
-  wget -O /tmp/vagrant_1.4.0_x86_64.deb https://dl.bintray.com/mitchellh/vagrant/vagrant_1.4.0_x86_64.deb
-  dpkg -i /tmp/vagrant_1.4.0_x86_64.deb
+  wget -O vagrant_1.6.2_x86_64.deb https://dl.bintray.com/mitchellh/vagrant/vagrant_1.6.2_x86_64.deb
+  dpkg -i /tmp/vagrant_1.6.2_x86_64.deb
 fi
 
