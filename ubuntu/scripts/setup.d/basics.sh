853b5d0d2823009a32b05e6737bad4f228cfa368
Jordi Beltran <beltrachi@gmail.com>
2014-09-18 23:07:19 +0200

    Ubuntu - Add compiz plugins to have Static app switcher on unity

--- ubuntu/scripts/setup.d/basics.sh
+++ ubuntu/scripts/setup.d/basics.sh
@@ -2,7 +2,7 @@
 apt-get install -y git-core chromium-browser curl openssh-server pidgin \
   build-essential indicator-multiload tree vim nautilus-dropbox \
   ttf-liberation vlc whois compizconfig-settings-manager \
-  pepperflashplugin-nonfree gparted
+  pepperflashplugin-nonfree gparted compiz-plugins
 
 update-rc.d ssh defaults
 
