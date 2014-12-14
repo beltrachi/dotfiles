475735098eff6187818ac9637e2af7219bf193fa
Jordi Beltran <beltrachi@gmail.com>
2014-12-14 20:59:27 +0100

    Update ubuntu setup for emacs to ubuntu elisp ppa

--- ubuntu/scripts/setup.d/emacs.sh
+++ ubuntu/scripts/setup.d/emacs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo add-apt-repository ppa:cassou/emacs
+sudo add-apt-repository ppa:ubuntu-elisp/ppa
 sudo apt-get update
 sudo apt-get install -y emacs24 emacs24-el emacs24-common-non-dfsg
