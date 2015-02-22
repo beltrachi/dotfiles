ad46e24e8e2d19090501f676c0dd9349bd392753
Jordi Beltran <beltrachi@gmail.com>
2015-02-22 23:45:18 +0100

    Add docker to ubuntu setup

--- ubuntu/scripts/setup.d/devel.sh
+++ ubuntu/scripts/setup.d/devel.sh
@@ -19,3 +19,18 @@ add-apt-repository -y ppa:chris-lea/node.js
 apt-get update
 apt-get install -y python-software-properties python g++ make nodejs
 npm install coffee-script -g
+
+[ -e /usr/lib/apt/methods/https ] || {
+  apt-get update
+  apt-get install apt-transport-https
+}
+
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+
+sh -c "echo deb https://get.docker.com/ubuntu docker main\
+> /etc/apt/sources.list.d/docker.list"
+
+apt-get update
+apt-get install lxc-docker
+
+curl -L https://github.com/docker/fig/releases/download/1.0.1/fig-`uname -s`-`uname -m` > /usr/local/bin/fig; chmod +x /usr/local/bin/fig
