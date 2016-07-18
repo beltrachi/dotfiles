f63c2c5ac23ac6d5b9fef19ca358a8b8f1dccd1f
Jordi Beltran <beltrachi@gmail.com>
2016-07-18 19:14:03 +0200

    Show error code returned by the command in bash

--- bash/long_command_notify.sh
+++ bash/long_command_notify.sh
@@ -1,7 +1,8 @@
 # Script to notify on slow commands
 beep_if_long_time_past() {
+    local LAST_EXIT="$?"
     LAST_COMMAND_DURATION=$(($(date +%s) - ${LAST_COMMAND_TIME}))
-    [[ ${LAST_COMMAND_DURATION} -gt 10 ]] && { notify-send "Script finished in ${LAST_COMMAND_DURATION} seconds" "$previous_command"; }
+    [[ ${LAST_COMMAND_DURATION} -gt 10 ]] && { notify-send "Script finished with ${LAST_EXIT} in ${LAST_COMMAND_DURATION} seconds" "$previous_command"; }
     export LAST_COMMAND_TIME=
 }
 
