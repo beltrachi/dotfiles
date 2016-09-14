c129a5799b81fd06989172c04bd031acb2bcb53d
Jordi Beltran <beltrachi@gmail.com>
2016-09-15 00:04:21 +0200

    add-apt-repository skip confirmation

--- ubuntu/scripts/setup.d/java.sh
+++ ubuntu/scripts/setup.d/java.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-sudo add-apt-repository ppa:webupd8team/java
+sudo add-apt-repository ppa:webupd8team/java -y
 sudo apt-get update
 sudo apt-get install -y oracle-java7-installer ant
 
