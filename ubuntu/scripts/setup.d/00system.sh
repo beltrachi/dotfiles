c8104a11913aad57c83b70497b0655cc17e7f148
Jordi Beltran <beltrachi@gmail.com>
2016-09-16 09:37:39 +0200

    Ubuntu setup - system hardware configrations

---
+++ ubuntu/scripts/setup.d/00system.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+P530=$(dmidecode |grep "P530-K.AE22B")
+
+if [[ -n "$P530" ]]; then
+	# Fix backlight
+	sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash acpi_backlight=vendor"/' /etc/default/grub 
+	update-grub
+
+ 	apt-get install nvidia-prime nvidia-361
+fi
+
+
