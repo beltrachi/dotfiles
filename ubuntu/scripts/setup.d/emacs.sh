ae9d10f017c37cb9077f7e1d04cc4f58f3bbaf3f
Jordi Beltran <beltrachi@gmail.com>
2016-10-25 23:48:25 +0200

    Migrate to spacemacs

--- ubuntu/scripts/setup.d/emacs.sh
+++ ubuntu/scripts/setup.d/emacs.sh
@@ -3,4 +3,5 @@ sudo add-apt-repository ppa:ubuntu-elisp/ppa -y
 sudo apt-get update
 sudo apt-get install -y emacs-snapshot
 
-wget https://github.com/bbatsov/prelude/raw/master/utils/installer.sh -O - | sh
+git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+
