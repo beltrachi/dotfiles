67607a28970d8164efb4abc8f233696a07d69c79
Jordi Beltran <beltrachi@gmail.com>
2017-03-28 00:08:38 +0200

    Disable internal wifi ifce

--- ubuntu/scripts/setup.d/00system.sh
+++ ubuntu/scripts/setup.d/00system.sh
@@ -9,6 +9,12 @@ if [[ -n "$P530" ]]; then
 
 	# Disabled nvidia because it was giving problems after rebooting a fresh installation
 	# apt-get install -y nvidia-prime nvidia-361
+
+	# Disable internal interface
+	LINE="iface wlp8s0 inet manual"
+	FILE="/etc/network/interfaces"
+	grep -q -F "$LINE" $FILE || echo "$LINE" >> $FILE
+	service network-manager restart
 fi
 
 
