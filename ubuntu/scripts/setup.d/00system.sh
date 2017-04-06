78e2a7849ddecd57053bd7dd6116f6032ff43a2c
Jordi Beltran <beltrachi@gmail.com>
2017-04-06 19:45:14 +0200

    Disable internal wifi

--- ubuntu/scripts/setup.d/00system.sh
+++ ubuntu/scripts/setup.d/00system.sh
@@ -10,12 +10,15 @@ if [[ -n "$P530" ]]; then
 	sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash acpi_backlight=vendor"/' /etc/default/grub 
 	update-grub
 
-
-	# Disable internal interface
-	LINE="iface wlp8s0 inet manual"
-	FILE="/etc/network/interfaces"
-	grep -q -F "$LINE" $FILE || echo "$LINE" >> $FILE
-	service network-manager restart
+	# When usb wifi detected, disable internal wifi device.
+	HAS_USB_WIFI=$(lsusb | grep "RTL8188CUS")
+	if [[ -n "$HAS_USB_WIFI" ]]; then
+		# Disable internal interface
+		LINE="iface wlp8s0 inet manual"
+		FILE="/etc/network/interfaces"
+		grep -q -F "$LINE" $FILE || echo "$LINE" >> $FILE
+		service network-manager restart
+	fi
 fi
 
 
