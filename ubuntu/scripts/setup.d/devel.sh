a3e94353ef4553afc6bcd27ccdbdab59b0697d5d
Jordi Beltran <beltrachi@gmail.com>
2017-06-09 13:02:32 +0200

    Add rbenv installation

--- ubuntu/scripts/setup.d/devel.sh
+++ ubuntu/scripts/setup.d/devel.sh
@@ -15,6 +15,20 @@ if [[ ! $(command -v vagrant) ]]; then
   fi
 fi
 
+# Install rbenv
+su -l `logname` <<'EOF'
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+source ~/.bashrc
+rbenv install 2.1.2
+rbenv global 2.1.2
+gem install bundler
+EOF
+
+exit 1
+# Nodejs
 add-apt-repository -y ppa:chris-lea/node.js
 apt-get update
 apt-get install -y python-software-properties python g++ make nodejs
@@ -32,5 +46,6 @@ sh -c "echo deb https://get.docker.com/ubuntu docker main\
 
 apt-get update
 apt-get install lxc-docker
+# TODO: add user to docker group
 
 curl -L https://github.com/docker/fig/releases/download/1.0.1/fig-`uname -s`-`uname -m` > /usr/local/bin/fig; chmod +x /usr/local/bin/fig
