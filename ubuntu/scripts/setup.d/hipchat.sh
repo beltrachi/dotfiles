12256643ee1f7afa6b9709830dc474418c0e8055
Jordi Beltran <beltrachi@gmail.com>
2018-09-30 17:27:46 +0200

    Fix hipchat setup for ubuntu 18.04

--- ubuntu/scripts/setup.d/hipchat.sh
+++ ubuntu/scripts/setup.d/hipchat.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-echo "deb http://downloads.hipchat.com/linux/apt stable main" > /etc/apt/sources.list.d/atlassian-hipchat.list
-wget -O - https://www.hipchat.com/keys/hipchat-linux.key | apt-key add -
+# Fixed command from hipchat page because bionic release is still not ready. Workardound is to use xenial
+echo "deb https://atlassian.artifactoryonline.com/atlassian/hipchat-apt-client xenial main" > /etc/apt/sources.list.d/atlassian-hipchat4.list
+wget -O - https://atlassian.artifactoryonline.com/atlassian/api/gpg/key/public | sudo apt-key add -
+
 apt-get update
-apt-get install -y hipchat
+apt-get install -y hipchat4
