6f332b9f43edc3e6507439d16d59e93cb6c29340
Jordi Beltran <beltrachi@gmail.com>
2019-04-25 12:38:08 +0200

    Fix bash completion on macos term

--- macos/bash_specifics.sh
+++ macos/bash_specifics.sh
@@ -5,6 +5,6 @@ function notify-send(){
   terminal-notifier -title Command -subtitle "$2" -message "$1"
 
 }
-
+[ -f /usr/local/etc/bash_completion ] && . /usr/local/etc/bash_completion
 source /usr/local/etc/bash_completion.d/git-prompt.sh
 
