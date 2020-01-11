2d6dac78d31fa4418735447f3ead0e8dd7ce38b7
Jordi Beltran <beltrachi@gmail.com>
2020-01-11 18:40:55 +0100

    Ubuntu - update spotify setup

--- ubuntu/scripts/setup.d/spotify.sh
+++ ubuntu/scripts/setup.d/spotify.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
-
-echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+#!/bin/bash -ex
+curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 
 sudo apt-get update
 sudo apt-get install -y spotify-client
