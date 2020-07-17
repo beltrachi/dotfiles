ddd2563e6720d348c93b87fcbefa31a8cf8ce01f
Jordi Beltran <beltrachi@gmail.com>
2020-07-17 10:40:48 +0200

    Fix macos setup

--- macos/setup/install.sh
+++ macos/setup/install.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
-set -ex
-
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 ./brew.sh
 ./.macos
+
+echo "Installation complete. Now we're going to execute some apps to be correctly setted up"
+
+echo "Open spotlight (Cmd + Space) and execute Spectacle. Follow the instructions."
+
+function press_any_key(){
+    read -n1 -r -p "Press any key to continue..." key
+}
+press_any_key
+
+echo "Open spotlight and execute Karabiner-elements"
+press_any_key
+
+echo "Open spotlight and execute MenuMeters"
+press_any_key
