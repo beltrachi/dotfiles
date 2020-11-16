60160df86e26b05fa73483aabb1a047bac5888cd
Jordi Beltran <beltrachi@gmail.com>
2020-11-16 23:03:55 +0100

    Fixing vscode alias

--- vscode/alias.sh
+++ vscode/alias.sh
@@ -6,12 +6,12 @@
 #   e               # Opens current directory
 
 # Mac vscodium binary is code
-if which vscode 2> /dev/null; then
+if which vscode 2>&1 > /dev/null; then
   true # Nothing to do.
 else
-  if which code 2> /dev/null; then
+  if which code 2>&1 > /dev/null; then
     vscode () { code $@; }
-  elif which codium 2> /dev/null; then
+  elif which codium 2>&1 > /dev/null; then
     vscode () { codium $@; }
   fi
 fi
