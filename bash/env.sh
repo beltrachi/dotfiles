8540f3c4ecccbfbf1f7b16e0d47a44217056ee7b
Jordi Beltran <beltrachi@gmail.com>
2024-08-07 17:12:00 +0200

    Remove RVM (we have rbenv)

--- bash/env.sh
+++ bash/env.sh
@@ -1,7 +1,1 @@
-if [ -x ~/.rvm/bin/rvm-prompt ]; then
-    RVM_PART='\[\e[0;31m\][$(~/.rvm/bin/rvm-prompt i v p g system)]'
-else
-    RVM_PART='\[\e[0;31m\][env=$RAILS_ENV]'
-fi
-export PS1='\[\033[1;90m\]\A \[\033[01;32m\]\u@\h\[\033[01;34m\] \W '$RVM_PART'\[\e[0;32m\] $(__git_ps1 "(%s)")\[\e[0;37m\] \$\[\033[00m\] '
-
+export PS1='\[\033[1;90m\]\A \[\033[01;32m\]\u@\h\[\033[01;34m\] \W\[\e[0;32m\] $(__git_ps1 "(%s)")\[\e[0;37m\] \$\[\033[00m\] '
